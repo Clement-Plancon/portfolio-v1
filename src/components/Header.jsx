b84574f0bdca32009226598e48d218216e5e6c70
Clement-Plancon <plancon.clem@gmail.com>
2022-06-27 15:03:25 +0200

    dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -91,8 +91,7 @@ const removeMenuText = () =>{
 const DarkLightMode = () =>{
 document.querySelector("body").classList.toggle('darkModeBody')
 document.querySelector(".header").classList.toggle('darkModeBody')
-document.querySelector(".header").classList.toggle('darkModeBody')
-document.querySelector(".header").classList.toggle('darkModeBody')
+document.querySelector(".mobileNavbar").classList.toggle('darkModeBody')
 document.querySelectorAll("h1, h2, p,.link a,.about .center .center__right a,.cardHardSkills__title h3").forEach(function(i){
 (i.classList.toggle('darkModeText'));
 });
@@ -154,7 +153,7 @@ document.querySelectorAll(".home__socialIcons svg, .header .link svg, .skills .c
             <path
               fill="currentColor"
               d="M11 19H8a1 1 0 0 0 0 2h3a1 1 0 0 0 0-2Zm9-4h-1.16A8.18 8.18 0 0 0 20 12.05a1 1 0 0 0-.34-.93a1 1 0 0 0-1-.19a6 6 0 0 1-1.92.32a6.06 6.06 0 0 1-6.06-6a6.93 6.93 0 0 1 .1-1a1 1 0 0 0-.35-.92a1 1 0 0 0-1-.18A8.06 8.06 0 0 0 4 10.68A8 8 0 0 0 5.27 15H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2Zm-3.72 0H7.83a6 6 0 0 1 .88-9.36a8.06 8.06 0 0 0 8.05 7.61a7 7 0 0 0 .79 0A6.08 6.08 0 0 1 16.28 15ZM16 19h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2Z"
-            />
+              onClick={() => DarkLightMode()} />
           </svg>
           <img
             src="./icon_global/menu-mobile.svg"
